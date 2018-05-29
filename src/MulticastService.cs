6f399c376ca2c8fcdd373ea5ed6e823b882ceb85
Richard Schneider <makaretu@gmail.com>
2018-05-29 23:01:46 +1200

    chore: upgrade dependencies

--- src/MulticastService.cs
+++ src/MulticastService.cs
@@ -1,13 +1,10 @@
 ﻿using Common.Logging;
-using Makaretu.Dns;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
