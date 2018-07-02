9b1ed944e7e70cbfaff72288b8860786f2a69ed2
Richard Schneider <makaretu@gmail.com>
2018-07-02 12:12:09 +1200

    test: no nics found

--- src/MulticastService.cs
+++ src/MulticastService.cs
@@ -133,6 +133,12 @@ namespace Makaretu.Dns
         /// </returns>
         protected static IEnumerable<NetworkInterface> GetNetworkInterfaces()
         {
+            foreach (var nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                Console.WriteLine($"NIC: ({nic.Id}) {nic.Name}");
+                Console.WriteLine($"     {nic.NetworkInterfaceType} {nic.OperationalStatus}");
+                Console.WriteLine($"     multicast {nic.SupportsMulticast}");
+            }
             return NetworkInterface.GetAllNetworkInterfaces()
                 .Where(nic => nic.OperationalStatus == OperationalStatus.Up)
                 .Where(nic => nic.NetworkInterfaceType != NetworkInterfaceType.Loopback && nic.NetworkInterfaceType != NetworkInterfaceType.Unknown)
