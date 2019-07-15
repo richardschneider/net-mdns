d479d164f7d3fe0a54e8cb0cc5138c6384e398f6
Richard Schneider <makaretu@gmail.com>
2019-07-15 16:55:16 +1200

    feat(traffic): a program to log MDNS traffic

---
+++ traffic/Program.cs
@@ -0,0 +1,43 @@
+﻿using Makaretu.Dns;
+using System;
+
+namespace traffic
+{
+    class Program
+    {
+        static readonly object ttyLock = new object();
+
+        static void Main(string[] args)
+        {
+            var mdns = new MulticastService();
+            mdns.NetworkInterfaceDiscovered += (s, e)
+                => mdns.SendQuery(ServiceDiscovery.ServiceName, type: DnsType.PTR);
+            mdns.AnswerReceived += OnGoodDnsMessage;
+            mdns.QueryReceived += OnGoodDnsMessage;
+            mdns.MalformedMessage += OnBadDnsMessage;
+            mdns.Start();
+            Console.ReadKey();
+        }
+
+        private static void OnBadDnsMessage(object sender, byte[] packet)
+        {
+            lock (ttyLock)
+            {
+                Console.WriteLine(">>> {0:O} <<<", DateTime.Now);
+                Console.WriteLine("Malformed message (base64)");
+                Console.WriteLine(Convert.ToBase64String(packet));
+            }
+
+            Environment.Exit(1);
+        }
+
+        private static void OnGoodDnsMessage(object sender, MessageEventArgs e)
+        {
+            lock (ttyLock)
+            {
+                Console.WriteLine("=== {0:O} ===", DateTime.Now);
+                Console.WriteLine(e.Message.ToString());
+            }
+        }
+    }
+}
